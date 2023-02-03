aea7c12316a797329e638ebb91ff50c6914107ca
Nayon <nayonroy64@gmail.com>
2023-02-03 19:18:02 +0600

    change history navbar backgroud color

--- src/Pages/Home/Transaction/RequestHistory/RequestHistory.js
+++ src/Pages/Home/Transaction/RequestHistory/RequestHistory.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 const RequestHistory = () => {
-	const linkRoutes = [
+  const linkRoutes = [
     { path: "/", name: "ETH Transaction History" },
     { path: "/h2", name: "TestLink Transaction History" },
-	];
-	const routes = (
+  ];
+  const routes = (
     <>
       {linkRoutes.map((route, i) => (
         <li key={i} className="text-decoration-none mt-2 mb-3">
@@ -14,7 +14,7 @@ const RequestHistory = () => {
             to={route.path}
             className={({ isActive }) =>
               isActive
-                ? "text-decoration-none  py-2 px-3 me-2 bg-primary fs-6 text-white rounded-1"
+                ? "text-decoration-none  py-2 px-3 me-2 bg-blue fs-6 text-white rounded-1"
                 : "text-decoration-none  py-2 px-3 me-2 bg-blue-10 fs-6 text-black rounded-1"
             }
           >
@@ -24,7 +24,7 @@ const RequestHistory = () => {
       ))}
     </>
   );
-	return (
+  return (
     <div className="mt-4 col-md-6">
       <h6 className="fw-bold ">Request History</h6>
       <ul className="p-0 list-style-none d-flex flex-wrap">{routes}</ul>
